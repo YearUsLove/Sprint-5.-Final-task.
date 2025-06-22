54f57323bd56fdfc47b41534323ca2de2f48fccd
Yaroslav <petruninyarpetrunin@yandex.ru>
2025-06-22 19:50:26 +0300

    Fifth sprint. Final task

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,32 @@
 package actioninfo
 
+import (
+	"fmt"
+	"log"
+)
+
 type DataParser interface {
 	// TODO: добавить методы
+	Parse(string) error
+	ActionInfo() (string, error)
 }
 
 func Info(dataset []string, dp DataParser) {
 	// TODO: реализовать функцию
+	for _, data := range dataset {
+		err := dp.Parse(data)
+		if err != nil {
+			log.Printf("ошибка парсинга: %v", err)
+			continue
+		}
+
+		info, err := dp.ActionInfo()
+		if err != nil {
+			log.Printf("ошибка получения информации: %v", err)
+			continue
+		}
+
+		fmt.Println(info)
+
+	}
 }
